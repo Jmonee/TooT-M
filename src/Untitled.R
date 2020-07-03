9463ddf1cc60b9ba2bdeebb58649c28710c3837b
Munira Alballa <monerah.alballaa@gmail.com>
2020-07-02 21:52:48 -0400

    deleted irrelative files

--- src/Untitled.R
+++
@@ -1,8 +0,0 @@
-getPsiAndKs<- function(features)
-{
-  
-  psi=as.integer(features/10.0001)
-  K<- as.integer(((features/10.0001)-(as.integer(features/10.0001))+.01)*10)
- All<- list(psi=psi, K=K)
-  return(All) 
-}
